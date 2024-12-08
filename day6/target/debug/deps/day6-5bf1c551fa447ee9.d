1fd844f5c60f8be488c12d7ba27861a457ebe52d
nessie710 <jacoponicolini7@gmail.com>
2024-12-08 12:26:33 +0100

    Day8 done yayyyyy

--- day6/target/debug/deps/day6-5bf1c551fa447ee9.d
+++
@@ -1,5 +0,0 @@
-/home/jacopo/Programming languages/Advent_of_code/advent_of_code_2024/day6/target/debug/deps/libday6-5bf1c551fa447ee9.rmeta: src/main.rs
-
-/home/jacopo/Programming languages/Advent_of_code/advent_of_code_2024/day6/target/debug/deps/day6-5bf1c551fa447ee9.d: src/main.rs
-
-src/main.rs:
--- day6/target/debug/deps/unicode_ident-0c791808698d0f3c.d
+++
@@ -1,8 +0,0 @@
-/home/jacopo/Programming languages/Advent_of_code/advent_of_code_2024/day6/target/debug/deps/libunicode_ident-0c791808698d0f3c.rmeta: /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/jacopo/Programming languages/Advent_of_code/advent_of_code_2024/day6/target/debug/deps/libunicode_ident-0c791808698d0f3c.rlib: /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/jacopo/Programming languages/Advent_of_code/advent_of_code_2024/day6/target/debug/deps/unicode_ident-0c791808698d0f3c.d: /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs:
-/home/jacopo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs:
