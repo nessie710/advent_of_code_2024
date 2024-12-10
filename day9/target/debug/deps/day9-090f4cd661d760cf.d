e96e23705c50ebd8413a85307e1a18a552c478f9
Jacopo Nicolini <jacoponicolini7@gmail.com>
2024-12-10 12:33:02 +0100

    Target deleted

--- day9/target/debug/deps/day9-090f4cd661d760cf.d
+++
@@ -1,5 +0,0 @@
-/home/jnicolini/advent_of_code_2024/day9/target/debug/deps/libday9-090f4cd661d760cf.rmeta: src/main.rs
-
-/home/jnicolini/advent_of_code_2024/day9/target/debug/deps/day9-090f4cd661d760cf.d: src/main.rs
-
-src/main.rs:
